2e8b11d1669df28bbc3a845c1596a27fc63ab62f
mdgulamasib <gulam.asib@gmail.com>
2022-04-16 22:58:54 +0600

    simple navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Shared/Header/Header';
+import Footer from './Shared/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Services from './Pages/Services/Services';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Assignment 10 Environment Setup</h1>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" id='navbar-styling' variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'><img src={logo} className='me-md-5' height={90} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className='me-md-3' as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link className='me-md-3' as={Link} to="/services">Services</Nav.Link>
+                            <Nav.Link className='me-md-3' href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link className='me-md-3' href="#about">About</Nav.Link>
+                            <Nav.Link href="#login">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
