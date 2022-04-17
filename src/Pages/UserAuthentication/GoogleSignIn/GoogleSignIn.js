9953486ae5aa574e175c7fd20a80d4b2f3c6ee05
mdgulamasib <gulam.asib@gmail.com>
2022-04-17 16:26:06 +0600

    google auth  login successful

--- src/Pages/UserAuthentication/GoogleSignIn/GoogleSignIn.js
+++ src/Pages/UserAuthentication/GoogleSignIn/GoogleSignIn.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './GoogleSignIn.css'
 import google_icon from '../../../images/google_icon.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import Loading from '../../../Shared/Loading/Loading';
+
 
 const GoogleSignIn = () => {
+    let errorElement;
+
+
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+    console.log(user)
+
+
+
     return (
         <div>
-            <button
+            {errorElement}
+            <button onClick={() => signInWithGoogle()}
                 className='btn mx-auto my-2 googlelogin-btn'>
                 <img src={google_icon} alt="" />
                 <span className='px-2'>Login Using Google Account</span>
